447f7ddeca6ef5204215affae2fc9781b36eaf27
devfardin <contactfardin22@gmail.com>
2025-03-19 15:05:59 +0600

    buy now button in single page

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@
 /**
  * Define Constants
  */
-define( 'CHILD_THEME_HAPPY_CARE_LIFE_VERSION', '1.0.0' );
+define('CHILD_THEME_HAPPY_CARE_LIFE_VERSION', '1.0.0');
 
 
 /**
@@ -21,33 +21,52 @@ define( 'CHILD_THEME_HAPPY_CARE_LIFE_VERSION', '1.0.0' );
  * @return void
  */
 
- require_once(  __DIR__ . '/includes/setup.php' );
-function register_new_widgets( $widgets_manager ) {
+require_once(__DIR__ . '/includes/setup.php');
+function register_new_widgets($widgets_manager)
+{
 
-	require_once( __DIR__ . '/includes/widgets/products.php' );
-	$widgets_manager->register( new \Elementor_Products_widget() );
+	require_once(__DIR__ . '/includes/widgets/products.php');
+	$widgets_manager->register(new \Elementor_Products_widget());
 
 }
-add_action( 'elementor/widgets/register', 'register_new_widgets' );
+add_action('elementor/widgets/register', 'register_new_widgets');
 
 /**
  * Enqueue styles
  */
-function child_enqueue_styles() {
+function child_enqueue_styles()
+{
 
-	wp_enqueue_style( 'happy-care-life-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_HAPPY_CARE_LIFE_VERSION, 'all' );
-	wp_register_style('happy_care_products_style', get_stylesheet_directory_uri()  . '/assets/css/products.css');
+	wp_enqueue_style('happy-care-life-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_HAPPY_CARE_LIFE_VERSION, 'all');
+	wp_register_style('happy_care_products_style', get_stylesheet_directory_uri() . '/assets/css/products.css');
 
 }
 
-add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+add_action('wp_enqueue_scripts', 'child_enqueue_styles', 15);
 
 
 
 // After Add to cart its redirect checkout page
-function redirect_to_checkout() {
-    global $woocommerce;
-    $checkout_url = $woocommerce->cart->get_checkout_url();
-    return $checkout_url;
+function redirect_to_checkout()
+{
+	global $woocommerce;
+	$checkout_url = $woocommerce->cart->get_checkout_url();
+	return $checkout_url;
 }
-add_filter ('add_to_cart_redirect', 'redirect_to_checkout');
+add_filter('add_to_cart_redirect', 'redirect_to_checkout');
+
+
+// in the single page add buy now button
+function single_product_add_buy_btn()
+{
+	global $product;
+	$pid = $product->get_id();
+	?>
+	<a href="<?php
+	echo do_shortcode('[add_to_cart_url id=' . $pid . ']') ?>"
+	>
+		অর্ডার করুন
+	</a>
+	<?php
+}
+add_action('woocommerce_after_main_content', 'single_product_add_buy_btn',);
